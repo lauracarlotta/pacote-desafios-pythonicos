4fe37eb984a6738364a3bf9b040237f9e2f1f1d7
Laura Carlotta <carlotta.custodio@gmail.com>
2024-04-01 11:07:53 -0300

    feat: execute ex03 and correct

--- 03_fix_start.py
+++ 03_fix_start.py
@@ -13,28 +13,53 @@ Dica: s.replace(stra, strb) retorna uma versão da string s
 onde todas as instancias de stra foram substituidas por strb.
 """
 
-def fix_start(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+# TODO usar o método replace
+# TODO usar outros métodos do python
+# TODO escrever em apenas uma linha
+# ==> Eliminar repetições, legibilidade, nomear variáveis com nomes semanticos e pequenos
 
+def fix_start(s):
+    # 1 - substitui e substitui de novo
+    # 2 - quantas ocorrencias aparecem daquela letra
+    # 3 - replace a partir do indice 1  <================
+    # +++ SUA SOLUÇÃO +++
+
+    # SOLUTION 3
+    # TODO - não consegui usar outro metodo
+    """value = s
+    value = s.replace(s[0], '*', value.find(value.index(s[0]), 1, len(s)))
+    return value"""
+
+    # SOLUTION 2
+    """new_string = ''
+    for cont, value in enumerate(s):
+        if cont == 0 or value != s[0]:
+            new_string += value
+        else:
+            new_string += value.replace(s[0], '*')
+
+    return new_string"""
+
+    # SOLUTION 1
+    return s[0] + s[1:].replace(s[0], '*') if len(s) > 1 else s
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
-def test(f, in_, expected):
+def test(function, input, expected):
     """
     Executa a função f com o parâmetro in_ e compara o resultado com expected.
     :return: Exibe uma mensagem indicando se a função f está correta ou não.
     """
-    out = f(in_)
+    output = function(input)
 
-    if out == expected:
+    if output == expected:
         sign = '✅'
         info = ''
     else:
         sign = '❌'
         info = f'e o correto é {expected!r}'
 
-    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+    print(f'{sign} {function.__name__}({input!r}) retornou {output!r} {info}')
 
 
 if __name__ == '__main__':
@@ -43,3 +68,4 @@ if __name__ == '__main__':
     test(fix_start, 'aardvark', 'a*rdv*rk')
     test(fix_start, 'google', 'goo*le')
     test(fix_start, 'donut', 'donut')
+    # test(fix_start, 'd', 'donut')
