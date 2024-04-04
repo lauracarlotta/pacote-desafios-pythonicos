225bfe8e2df6450871143d07aeb75974304f09fa
Laura Carlotta <carlotta.custodio@gmail.com>
2024-04-04 16:57:18 -0300

    feat: add file with explanation about: .split, .join and enumerate methods

--- count_join_find_index.py
+++ count_join_find_index.py
@@ -59,5 +59,103 @@ print('--------------------------------')
 # ==> Qual o primeiro índice de x (com a variante de find, de que quando o valor não é encontrado, estoura o erro 'ValueError')
 
 print('ocorrencias de "ban": ', nome.index('ban')) # indice 4
-print('[ERROR] - ocorrencias de "x": ', nome.index('x')) # ValueError: substring not found
+# print('[ERROR] - ocorrencias de "x": ', nome.index('x')) # ValueError: substring not found
 print('--------------------------------')
+
+
+
+# ---------------------------------------
+print('--------------------------------')
+print('----------- JOIN ---------------')
+print('--------------------------------')
+# ---------------------------------------
+# Junta uma lista (retorna uma string - .join() funciona apenas com itens iteraveis.
+
+variable1 = 'O Brasil é um país tropical, o Brasil é o país do futebol, o Brasil é penta campeão.'
+list_split = variable1.split(' ')
+print(variable1)
+print(list_split)
+list_split.append('aaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+list_split_with_join = ' '.join(list_split)
+print(list_split_with_join)
+
+
+
+# ---------------------------------------
+print('--------------------------------')
+print('----------- SPLIT --------------')
+print('--------------------------------')
+# ---------------------------------------
+# Divide uma string
+
+variable = 'O Brasil é um país tropical, o Brasil é o país do futebol, o Brasil é penta campeão.'
+print('fatiamento da variável strin acima com split:')
+items_list_1 = variable.split(' ')
+for item in items_list_1:
+	print(item)
+print(f'items_list_1 tem {len(items_list_1)} elementos.')
+
+# =============================
+word = ''
+times = 0
+for item in items_list_1:
+	step = items_list_1.count(item)
+
+	if step > times:
+		times = step
+		word = item
+
+print(f'A palavra {word} apareceu {times}x')
+# =============================
+items_list_2 = variable.split(',')
+print(items_list_2)
+print(f'items_list_2 tem {len(items_list_2)} elementos.')
+for item in items_list_2:
+	print(item.strip().title())
+
+
+
+# ---------------------------------------
+print('--------------------------------')
+print('--------- ENUMERATE ------------')
+print('--------------------------------')
+# ---------------------------------------
+# Enumera elementos da lista
+# Desempacota itens de uma lista
+
+variable2 = 'O Brasil é um país tropical, o Brasil é o país do futebol, o Brasil é penta campeão.'
+items_list_3 = variable2.split(' ')
+for index, value in enumerate(items_list_3):
+	print(index, value)
+
+# =============================
+
+new_list = [
+	[1, 2],
+	[3, 4],
+	[5, 6]
+]
+
+for value in new_list:
+	print(value)
+	print(value[0])
+	print(value[1])
+
+for value in new_list:
+	for index1 in value:
+		print(index1, '-', value)
+
+
+new_new_list = [
+	[30, 'Laura'],
+	[59, 'Rubens'],
+	[53, 'Lucinha']
+]
+
+for age, name in new_new_list:
+	print(f'{name} tem {age} anos.')
+
+# Desempacotamento
+names = ['helena', 'madalena', 'tereza']
+n1, n2, n3 = names
+print(n2)
